36db5bc5a2e88af61a847837435e35f802d7db79
robert-virtual <robertocastill63@gmail.com>
2022-05-19 00:22:06 -0600

    fetch

--- src/About.svelte
+++ src/About.svelte
@@ -1,3 +1,13 @@
-
+<script>
+    let quotePromise = fetch("https://programming-quotes-api.herokuapp.com/quotes/random").then(x=>x.json())
+</script>
 
 <h1>About</h1>
+{#await quotePromise}
+<p>loading...</p>
+{:then quote}
+<div>
+<p>{quote.en}</p>
+<p>{quote.author}</p>
+</div>
+{/await}
